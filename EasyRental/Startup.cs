be111f0051d067cf323f479429daf827435f09eb
faiaz000 <faiaz000@hotmail.com>
2017-11-12 21:18:44 +0600

    Add project files.

---
+++ EasyRental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EasyRental.Startup))]
+namespace EasyRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
